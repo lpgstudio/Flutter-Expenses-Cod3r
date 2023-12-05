c0aaf058bb54cefc6e7610919f6fa6fb2a4f4873
Thiago de Paula <thiago_newdesign@hotmail.com>
2023-12-05 12:56:47 -0300

    First commit

---
+++ android/app/src/main/kotlin/br/com/lpcode/expenses/expenses/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.lpcode.expenses.expenses
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        centerTitle: true,
+        toolbarHeight: 110,
+        titleSpacing: 00.0,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(25),
+              bottomLeft: Radius.circular(25)),
+        ),
+        title: const Text(
+          'Expenses',
+        ),
+      ),
+      body: const Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            height: 120,
+            child: Card(
+              child: Text('Grafico'),
+            ),
+          ),
+          Card(
+            child: Text('Lista de transações'),
+          ),
+        ],
+      ),
+    );
+  }
+}
